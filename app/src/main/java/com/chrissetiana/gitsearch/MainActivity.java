85ca522a1a4b3ca8c2f96f0b82d9f457fa308e6c
chrissetiana <chrissetiana@gmail.com>
2018-07-25 02:21:08 +0100

    Add custom class

--- app/src/main/java/com/chrissetiana/gitsearch/MainActivity.java
+++ app/src/main/java/com/chrissetiana/gitsearch/MainActivity.java
@@ -11,21 +11,3 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 }
-
-/*
-https://api.github.com/users/chrissetiana
-
-img avatar_url""
-name name""
-bio bio""
-repositories public_repos# -> url html_url""
-gists public_gists# -> https://gist.github.com/
-followers followers#
-following following#
-
-this is for each repo:
-https://api.github.com/users/chrissetiana/repos
-repo full_name"" (e.g. chrissetiana/acdss_ || name"" (e.g. acdss)
-language language""
-
- */
---
+++ app/src/main/java/com/chrissetiana/gitsearch/UserActivity.java
@@ -0,0 +1,86 @@
+package com.chrissetiana.gitsearch;
+
+public class UserActivity {
+
+    /*
+https://api.github.com/users/chrissetiana
+
+img avatar_url""
+name name""
+bio bio""
+repositories public_repos# -> url html_url""
+gists public_gists# -> https://gist.github.com/
+followers followers#
+following following#
+
+this is for each repo:
+https://api.github.com/users/chrissetiana/repos
+repo full_name"" (e.g. chrissetiana/acdss_ || name"" (e.g. acdss)
+language language""
+
+ */
+
+    private String userimage;
+    private String username;
+    private String bio;
+    private int repo;
+    private int gist;
+    private int ufollowers;
+    private int ufollowing;
+    private String repoName;
+    private String repoLanguage;
+    private String repoLink;
+
+    public UserActivity(String userimage, String username, String bio, int repo, int gist, int ufollowers, int ufollowing, String repoName, String repoLanguage, String repoLink) {
+        this.userimage = userimage;
+        this.username = username;
+        this.bio = bio;
+        this.repo = repo;
+        this.gist = gist;
+        this.ufollowers = ufollowers;
+        this.ufollowing = ufollowing;
+        this.repoName = repoName;
+        this.repoLanguage = repoLanguage;
+        this.repoLink = repoLink;
+    }
+
+    public String getUserimage() {
+        return userimage;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public int getRepo() {
+        return repo;
+    }
+
+    public int getGist() {
+        return gist;
+    }
+
+    public int getUfollowers() {
+        return ufollowers;
+    }
+
+    public int getUfollowing() {
+        return ufollowing;
+    }
+
+    public String getRepoName() {
+        return repoName;
+    }
+
+    public String getRepoLanguage() {
+        return repoLanguage;
+    }
+
+    public String getRepoLink() {
+        return repoLink;
+    }
+}
