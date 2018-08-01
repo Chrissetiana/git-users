4185dd8e45a53944d2d7671114ecdd23c75167f3
chrissetiana <chrissetiana@gmail.com>
2018-08-01 22:29:40 +0100

    Add async task

--- app/src/main/java/com/chrissetiana/gitusers/MainActivity.java
+++ app/src/main/java/com/chrissetiana/gitusers/MainActivity.java
@@ -2,11 +2,15 @@ package com.chrissetiana.gitusers;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -42,5 +46,36 @@ public class MainActivity extends AppCompatActivity {
                 Log.d("MainActivity", "Redirecting to " + uri.toString());
             }
         });
+
+        ImageButton searchButton = findViewById(R.id.search_button);
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText searchText = findViewById(R.id.search_text);
+                String searchQuery = source + searchText.toString().trim();
+
+                UserAsyncTask task = new UserAsyncTask();
+                task.execute(searchQuery);
+
+                Log.d("MainActivity", searchQuery);
+            }
+        });
+    }
+
+    class UserAsyncTask extends AsyncTask<String, Void, UserActivity> {
+
+        @Override
+        protected UserActivity doInBackground(String... strings) {
+            if(strings[0] == null) {
+                return null;
+            }
+
+            return UserQuery.fetchData(strings[0]);
+        }
+
+        @Override
+        protected void onPostExecute(UserActivity userActivity) {
+            super.onPostExecute(userActivity);
+        }
     }
 }
